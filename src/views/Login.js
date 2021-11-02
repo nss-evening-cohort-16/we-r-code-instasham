c950093111a411b7196911cf5454d213c9e39a84
albertchitta <albert.w.chittaphong@gmail.com>
2021-11-01 21:47:47 -0500

    Updated Login styling

--- src/views/Login.js
+++ src/views/Login.js
@@ -4,9 +4,20 @@ import styled from 'styled-components';
 import { signInUser } from '../helpers/auth';
 
 const LoginStyle = styled.div`
-  width: 300px;
-  height: 140px;
-  margin: 300px auto;
+  width: 400px;
+  height: 240px;
+  display: flex;
+  margin: 200px auto;
+
+  .card {
+    border-radius: 10px;
+    border-color: #56d1ba;
+    width: 400px;
+    height: 240px;
+    box-shadow: 5px 10px 18px #888888;
+    padding: 10px;
+    padding-top: 40px;
+  }
 
   img {
     width: 100%;
@@ -14,8 +25,17 @@ const LoginStyle = styled.div`
   }
 
   button {
-    width: 300px;
+    width: 100%;
     margin-top: 40px;
+    background: white;
+    color: #56d1ba;
+    border-color: #56d1ba;
+  }
+
+  button:hover {
+    background-color: #ffcc99;
+    color: white;
+    border-color: #ffcc99;
   }
 `;
 
@@ -25,7 +45,7 @@ export default function SignIn({ user }) {
       {user === null ? (
         <h1>Loading...</h1>
       ) : (
-        <div className="text-center mt-5">
+        <div className="text-center mt-5 card">
           <img
             src="https://firebasestorage.googleapis.com/v0/b/instasham-bb5ca.appspot.com/o/instasham.png?alt=media&amp;token=97d55556-6ddb-4bb8-8067-7c31a90bf467"
             alt="Cluttered Couch"
