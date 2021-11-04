4471e76f538d3ec2bf39232c82277a2d5c04cb83
Grace Sutherland <rgs1690@gmail.com>
2021-11-03 19:01:14 -0500

    can search users on browse page

--- src/views/BrowseView.js
+++ src/views/BrowseView.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UsersList from '../components/instasham-design-system/UsersList';
 import USERJSON from '../sample_json/users.json';
 
+const getSearchItems = (searchTerm, users) => {
+  if (!searchTerm) {
+    return users;
+  }
+  return users.filter((user) => user.username.includes(searchTerm));
+};
 export default function BrowseView() {
+  const users = Object.values(USERJSON);
+  const [searchTerm, setSearchTerm] = useState('');
+  const searchItems = getSearchItems(searchTerm, users);
   return (
-    <div>
-      <UsersList userList={Object.values(USERJSON)} />
-    </div>
+    <>
+      <div className="input-group rounded">
+        <span className="input-group-text border-0" id="search-addon">
+          <i className="fas fa-search" />
+        </span>
+        <input
+          type="text"
+          className="form-control rounded"
+          placeholder="Search Users"
+          aria-label="Search Users"
+          aria-describedby="search-users"
+          onChange={(e) => { setSearchTerm(e.target.value); }}
+        />
+      </div>
+      <div>
+        <UsersList userList={searchItems} />
+      </div>
+    </>
   );
 }
