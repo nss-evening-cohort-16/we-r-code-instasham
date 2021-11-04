ef8a3e7de5763cde0691ed97c2bd7e2e7ef6a1d3
Grace Sutherland <rgs1690@gmail.com>
2021-11-03 19:16:52 -0500

    tried styling

--- src/views/BrowseView.js
+++ src/views/BrowseView.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import UsersList from '../components/instasham-design-system/UsersList';
 import USERJSON from '../sample_json/users.json';
 
+const SearchBarStyle = styled.div`
+.input-group {
+width: 100% !important;
+}
+`;
 const getSearchItems = (searchTerm, users) => {
   if (!searchTerm) {
     return users;
@@ -14,19 +20,21 @@ export default function BrowseView() {
   const searchItems = getSearchItems(searchTerm, users);
   return (
     <>
-      <div className="input-group rounded">
-        <span className="input-group-text border-0" id="search-addon">
-          <i className="fas fa-search" />
-        </span>
-        <input
-          type="text"
-          className="form-control rounded"
-          placeholder="Search Users"
-          aria-label="Search Users"
-          aria-describedby="search-users"
-          onChange={(e) => { setSearchTerm(e.target.value); }}
-        />
-      </div>
+      <SearchBarStyle>
+        <div className="input-group rounded">
+          <span className="input-group-text border-0" id="search-addon">
+            <i className="fas fa-search" />
+          </span>
+          <input
+            type="text"
+            className="form-control rounded"
+            placeholder="Search Users"
+            aria-label="Search Users"
+            aria-describedby="search-users"
+            onChange={(e) => { setSearchTerm(e.target.value); }}
+          />
+        </div>
+      </SearchBarStyle>
       <div>
         <UsersList userList={searchItems} />
       </div>
