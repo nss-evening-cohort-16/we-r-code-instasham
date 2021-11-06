2b2e48fa891c7d9349c3860b8740324e1659c5d9
Grace Sutherland <rgs1690@gmail.com>
2021-11-06 10:16:06 -0500

    deleted comment

--- src/views/PostDetailsView.js
+++ src/views/PostDetailsView.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-// import PropTypes from 'prop-types';
 import PostDetailsCard from '../components/instasham-design-system/PostDetailsCard';
 import { getSinglePost } from '../helpers/postHelper';
 
