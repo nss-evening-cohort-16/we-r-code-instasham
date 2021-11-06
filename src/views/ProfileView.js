442a9e6b8c2cdff9922ad24cc3733d90c51285dc
Grace Sutherland <rgs1690@gmail.com>
2021-11-06 11:03:19 -0500

    started

--- src/views/ProfileView.js
+++ src/views/ProfileView.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ProfileInfo from '../components/instasham-design-system/ProfileInfo';
 import PostGrid from '../components/instasham-design-system/PostGrid';
 import POSTJSON from '../sample_json/posts.json';
+import { getCurrentUsersUid, getUserByUid } from '../helpers/userHelper';
 
 export default function ProfileView() {
+  const { user, setUser } = useState({});
   const { username } = useParams();
+  useEffect(() => {
+    const userUid = getCurrentUsersUid();
+    getUserByUid(userUid).then(setUser);
+    console.warn(user);
+  }, []);
   return (
     <div>
       <h1>@{username}</h1>
